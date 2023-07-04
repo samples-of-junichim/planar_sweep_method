7229fa83c2fbf9d628804f3fac86e23894aca41e
junichim <junichim@users.noreply.github.com>
2023-07-04 09:12:39 +0900

    複数のテストで共通するクラスをモジュールにまとめる

--- test/test_TwoThreeTree_insert.py
+++ test/test_TwoThreeTree_insert.py
@@ -1,25 +1,7 @@
 from dataclasses import dataclass
 import unittest
 from TwoThreeTree import Leaf, Node, TwoThreeTree
-
-# テスト用の Node 要素
-@dataclass
-class NodeForTest:
-    id: str     # id
-    key: float  # キー
-
-class MyLeaf(Leaf[NodeForTest]):
-
-    def __init__(self, val: NodeForTest, parent: Node[NodeForTest] | None):
-        super().__init__(val, parent, get_key, comp_key)
-
-def get_key(v: NodeForTest) -> float:
-    return v.key
-def comp_key(v1: NodeForTest, v2: NodeForTest) -> bool:
-    return v1.key == v2.key
-def myleaf_ctor(v: NodeForTest, parent: Node[NodeForTest]):
-    return MyLeaf(v, parent)
-
+from test.TestClasses import MyLeaf, NodeForTest, comp_key, get_key, myleaf_ctor
 
 class TestTwoThreeTree(unittest.TestCase):
     """2-3 木に関する挿入操作のテスト
