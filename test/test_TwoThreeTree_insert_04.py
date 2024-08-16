747f21aff62b938bbca46761fe07971be2630f05
junichim <junichim@users.noreply.github.com>
2024-08-16 10:36:49 +0900

    2-3木の葉の追加テストに、順番が異なる場合を追加

--- test/test_TwoThreeTree_insert_04.py
+++ test/test_TwoThreeTree_insert_04.py
@@ -44,3 +44,58 @@ class TestTwoThreeTree(unittest.TestCase):
 
         self.tht.insert(NodeForTest("07", 4))
         self.tht.visualizeGraph(True, "sample_tree_const_7_" + datetime.datetime.now().isoformat())
+
+        self.assertEqual(11, self.tht.size)
+        self.assertEqual(7, self.tht.leafSize)
+        self.assertEqual(3, self.tht.height)
+
+    def test_create_tree_2(self):
+        """木の構築テスト
+
+        test_create_tree_3 との比較を行う
+        """
+        self.tht.insert(NodeForTest("01", -0.5))
+        self.tht.insert(NodeForTest("01", 1.25))
+        self.tht.insert(NodeForTest("01", 0))
+        self.tht.insert(NodeForTest("01", 1))
+
+        self.tht.visualizeGraph(True, "sample_tree_const_t2_1_" + datetime.datetime.now().isoformat())
+
+        self.tht.insert(NodeForTest("01", 0.5))
+
+        self.tht.visualizeGraph(True, "sample_tree_const_t2_2_" + datetime.datetime.now().isoformat())
+
+        self.tht.insert(NodeForTest("01", 1.5))
+
+        self.tht.visualizeGraph(True, "sample_tree_const_t2_3_" + datetime.datetime.now().isoformat())
+
+        # leaf が 2 の中間ノードが 3 つ
+        self.assertEqual(10, self.tht.size)
+        self.assertEqual(6, self.tht.leafSize)
+        self.assertEqual(3, self.tht.height)
+
+    def test_create_tree_3(self):
+        """木の構築テスト
+
+        test_create_tree_2 と同じ葉の要素だが、
+        追加する順番が異なると、結果の木が異なる
+        """
+        self.tht.insert(NodeForTest("01", -0.5))
+        self.tht.insert(NodeForTest("01", 0.5))
+        self.tht.insert(NodeForTest("01", 1))
+        self.tht.insert(NodeForTest("01", 1.25))
+
+        self.tht.visualizeGraph(True, "sample_tree_const_t3_1_" + datetime.datetime.now().isoformat())
+
+        self.tht.insert(NodeForTest("01", 0))
+
+        self.tht.visualizeGraph(True, "sample_tree_const_t3_2_" + datetime.datetime.now().isoformat())
+
+        self.tht.insert(NodeForTest("01", 1.5))
+
+        self.tht.visualizeGraph(True, "sample_tree_const_t3_3_" + datetime.datetime.now().isoformat())
+
+        # leaf が 3 の中間ノードが 2 つ
+        self.assertEqual(9, self.tht.size)
+        self.assertEqual(6, self.tht.leafSize)
+        self.assertEqual(3, self.tht.height)
